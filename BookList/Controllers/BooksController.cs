b3c21033d6fd5b41b410d384ebd0f4ec39522364
Biswa Nath Ghosh <tapos.aa@gmail.com>
2018-05-17 19:10:57 +0600

    initial commit done

---
+++ BookList/Controllers/BooksController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookList.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookList.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
